02591e329a755d09f433664be34a91c93c80cf2c
chengxiaolv <972987164@qq.com>
2019-02-15 14:30:43 +0800

    优化显示

--- src/common_assemblys_example/uploadFile_example.vue
+++ src/common_assemblys_example/uploadFile_example.vue
@@ -8,19 +8,39 @@
     
     <uploadFile :uploadFileSet='uploadFileSet' @getFileList="getFileList"></uploadFile>
 
+    <br>
+    <br>
+    <br>
+    
+
+    <uploadFile :uploadFileSet='uploadFileSet2' @getFileList="getFileList2"></uploadFile>
+
     
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
   export default {
     data () {
         return{
-					uploadFileSet:{ // 上传组件的配置项
+					uploadFileSet:{ // 上传组件的配置项   单文件  只能上传：jpg、jpeg、png、pdf
 						action: '/service?platform=web&command=uploadFile&data=%7B"command" : "uploadFile", "platform" : "web","type":"accountManage"%7D', //上传文件中 上传文件的后台地址 (必填)
 
 						uploadMore: 'one',  //上传文件中 控制是上传单文件还是多文件 (选填，默认为 ： 多文件上传   more:多文件，one：单文件)
+						btnInformation: '上传文件',  //上传文件中 按键的文字。 (选填，默认文字为 ： 上传文件)
+						uploadFileNum: 10, //上传文件中 上传文件的个数限制 (选填，默认个数为 ： 10)
+						uploadFormat: true, // 上传文件中 上传文件的格式设置 (选填，false为不做格式限制，true为设置为只可以为：jpg、jpeg、png、pdf， 默认为false)
+						btnSet:{  // 上传文件中 上传文件的按键样式控制 (选填，有默认值)
+							color: '',
+							fontSize: '',
+							// 这里可以设置按键的一切设置
+						}
+					},
+
+					uploadFileSet2:{ // 上传组件的配置项  多文件   上传一切文件
+						action: '/service?platform=web&command=uploadFile&data=%7B"command" : "uploadFile", "platform" : "web","type":"accountManage"%7D', //上传文件中 上传文件的后台地址 (必填)
+
+						uploadMore: 'more',  //上传文件中 控制是上传单文件还是多文件 (选填，默认为 ： 多文件上传   more:多文件，one：单文件)
 						btnInformation: '上传文件',  //上传文件中 按键的文字。 (选填，默认文字为 ： 上传文件)
 						uploadFileNum: 10, //上传文件中 上传文件的个数限制 (选填，默认个数为 ： 10)
 						uploadFormat: false, // 上传文件中 上传文件的格式设置 (选填，false为不做格式限制，true为设置为只可以为：jpg、jpeg、png、pdf， 默认为false)
@@ -34,6 +54,9 @@
     },
     methods: {
 			getFileList(fileList){ // 获取上传文件的集合。
+				console.log(fileList);
+			},
+			getFileList2(fileList){ // 获取上传文件的集合。
 				console.log(fileList);
 			},
 			loginClick () {
